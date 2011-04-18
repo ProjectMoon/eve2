9eae3abe059e3817ea9e16eda143714ee03fcc81
ProjectMoon <knifed@gmail.com>
2011-04-18 12:40:04 -0400

    clean up notes a bit

--- src/eve/core/EveCore.java
+++ src/eve/core/EveCore.java
@@ -60,36 +60,7 @@ public class EveCore {
 			//object scope is defined when proto is created.
 			//function scope is during execution of a function.
 			//single threaded, so only one scope, set globally by the interpreter.
-		
-		/*
-		 * Tree parser:
-		 * Think state machine. Use list to keep track of statements, and clear out
-		 * when switching scope. Maybe too simplistic. Keep list of statements per scope?
-		 * Keep list of statements only for functions. But we need to make sure it is clear
-		 * so only function statements get into it. What will make it stay clear?
-		 * 
-		 * Need to clear() pretty much everywhere, but only if not in a function.
-		 * But we don't know if we're in a function until we've popped up the tree
-		 * and figured out we need to make a function!
-		 * 
-		 * May need to subclass TreeAdaptor and provide a method to determine if we are
-		 * in a function (i.e. getParent() == FUNCTION_BODY).
-		 * 
-		 * Keep in mind that everything is bottom-up! We cannot assume that we are in the
-		 * logically correct scope when creating certain statements.
-		 * !!!We must remember that scope will be set AFTER we process statements in that scope!!!!
-		 * 
-		 * !!!!!*******VICTORY*******!!!!!!!!
-		 * During intial pass, all statements go into statement queue.
-		 * Function eval clears out statement queue and adds statements to the FunctionExpression.
-		 * FunctionExpression gets put into function queue.
-		 * Proto clears out statement queue AND function queue, adding all of them as code.
-		 * Proto is added to proto queue.
-		 * At EOF, global obj clears all three queues, adding every single thing as code.
-		 * Script is now in memory, and can be executed. <-- can add AoT "compile" step here.
-		 * ScopeManager is used during execution to keep track of call stack.
-		 */
-		
+				
 		/*
 		 * Separate execution tree:
 		 * We would need a separate ChangeScope statement that gets created by the tree parser.
@@ -105,31 +76,9 @@ public class EveCore {
 		 * does the execution tree need to be a tree? with changescope statement, we can flatten...
 		 * in theory.
 		 * 
-		 */
-		
-		/*
-		 * Statements:
-		 * EveStatement: interface with public EveObject execute();
-		 * AssignmentStatement(EveObject eo, ExpressionStatement expr);
-		 * FunctionDefStatement(EveObject protoScope, EveFunction func);
-		 * ProtoStatement(List<EveStatement> stmts);
-		 * FunctionInvokeStatement(); <-- nothing because there will be scope, right?!
-		 */
-		
-		/*
-		 * ScopeManager:
-		 * 	global (technically another object scope).
-		 * 	object
-		 * 	function
-		 */
-		
-		/*
-		 * Reference other scopes with scopeIdent::. global::func(), obj::method().
-		 * Provide self.x as alias for obj::x
-		 */
-		
-		/*
-		 * Global is an uncloneable prototype. 
+		 * execution tree is simply a list of Statements, but functions collect Statements themselves.
+		 * Prototypes will do the same.
+		 * 
 		 */
 	}
 }
