4787e9dce872bdc0a728bb691387dbb5f4c68470
projectmoon <noone@nowhere.net>
2011-04-25 20:07:31 -0400

    .class property for java types

--- src/eve/core/builtins/EveJava.java
+++ src/eve/core/builtins/EveJava.java
@@ -1,7 +1,13 @@
 package eve.core.builtins;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+
 import eve.core.EveObject;
+import eve.eni.EveNativeFunction;
+import eve.eni.NativeCode;
 import eve.eni.NativeHelper;
+import eve.scope.ScopeManager;
 
 /**
  * 	The java prototype.
@@ -18,7 +24,54 @@ public class EveJava extends EveObject {
 	private EveJava() {
 		this.setTypeName("java");
 		this.setType(EveType.PROTOTYPE);
-		this.putField("create", NativeHelper.javaFunction());
-		//this.putField("class", )
+		this.putField("create", javaFunction());
+	}
+	
+	private static EveObject javaFunction() {
+		final NativeCode nc = new NativeCode() {
+			@Override
+			public EveObject execute() {
+				EveObject cname = ScopeManager.getVariable("cname");
+				String className = cname.getStringValue();
+				EveObject ctorArgs = ScopeManager.getVariable("ctorArgs");
+							
+				try {
+					Constructor<?> ctor = NativeHelper.findConstructor(Class.forName(className), ctorArgs.getListValue());
+					Object o = ctor.newInstance(NativeHelper.mapToJava(ctorArgs.getListValue()));
+					EveObject eo = EveObject.javaType(o);
+					eo.putField("class", new EveObject(o.getClass().getName()));
+					eo.setTypeName(o.getClass().getName());
+					return eo;
+				}
+				catch (ClassNotFoundException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				catch (InstantiationException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				catch (IllegalAccessException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				catch (InvocationTargetException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				catch (SecurityException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				return null;
+			}		
+		};
+		
+		EveNativeFunction nfunc = new EveNativeFunction(nc);
+		nfunc.addParameter("cname");
+		nfunc.addParameter("ctorArgs");
+		nfunc.setVarargs(true);
+		nfunc.setVarargsIndex(1); //ctorArgs ...
+		return new EveObject(nfunc);
 	}
 }
--- src/eve/eni/NativeHelper.java
+++ src/eve/eni/NativeHelper.java
@@ -1,15 +1,12 @@
 package eve.eni;
 
 import java.lang.reflect.Constructor;
-import java.lang.reflect.InvocationTargetException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 import eve.core.EveError;
 import eve.core.EveObject;
-import eve.core.Function;
-import eve.scope.ScopeManager;
 
 public class NativeHelper {
 	public static Constructor<?> findConstructor(Class<?> cl, List<EveObject> params) {
@@ -35,7 +32,7 @@ public class NativeHelper {
 		throw new EveError("could not find constructor for " + cl.getName() + " matching " + params);
 	}
 	
-	private static Object[] mapToJava(List<EveObject> ctorArgs) {
+	public static Object[] mapToJava(List<EveObject> ctorArgs) {
 		Object[] args = new Object[ctorArgs.size()];
 		
 		for (int c = 0; c < args.length; c++) {
@@ -45,47 +42,5 @@ public class NativeHelper {
 		return args;
 	}
 	
-	public static EveObject javaFunction() {
-		final NativeCode nc = new NativeCode() {
-			@Override
-			public EveObject execute() {
-				EveObject cname = ScopeManager.getVariable("cname");
-				String className = cname.getStringValue();
-				EveObject ctorArgs = ScopeManager.getVariable("ctorArgs");
-							
-				try {
-					Constructor<?> ctor = findConstructor(Class.forName(className), ctorArgs.getListValue());
-					Object o = ctor.newInstance(mapToJava(ctorArgs.getListValue()));
-					EveObject eo = EveObject.javaType(o);
-					return eo;
-				} catch (ClassNotFoundException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				} catch (InstantiationException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				} catch (IllegalAccessException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				} catch (IllegalArgumentException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				} catch (InvocationTargetException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				} catch (SecurityException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-				return null;
-			}		
-		};
-		
-		EveNativeFunction nfunc = new EveNativeFunction(nc);
-		nfunc.addParameter("cname");
-		nfunc.addParameter("ctorArgs");
-		nfunc.setVarargs(true);
-		nfunc.setVarargsIndex(1); //ctorArgs ...
-		return new EveObject(nfunc);
-	}
+
 }
