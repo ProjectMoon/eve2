a32014eeb216d1a0571ae7f78910c0c475c70fce
projectmoon <noone@nowhere.net>
2011-05-15 12:53:44 -0400

    refactor java function to allow both exposing of types and not. added java::exposeType (delegates to JavaFunction)

--- src/eve/eji/stdlib/Java.java
+++ src/eve/eji/stdlib/Java.java
@@ -1,7 +1,5 @@
 package eve.eji.stdlib;
 
-import java.lang.reflect.Constructor;
-import java.lang.reflect.InvocationTargetException;
 import java.util.Map;
 
 import eve.core.EveError;
@@ -12,72 +10,12 @@ import eve.eji.EJIHelper;
 import eve.scope.ScopeManager;
 
 public class Java {
+
 	public static void init() {
 		ScopeManager.setNamespace("java");
 		ScopeManager.createGlobalScope();
-		ScopeManager.putVariable("expose", javaFunction());
+		ScopeManager.putVariable("expose", new EveObject(new JavaFunction(false)));
+		ScopeManager.putVariable("exposeType", new EveObject(new JavaFunction(true)));
 		ScopeManager.revertNamespace();
-	}
-	
-	private static EveObject javaFunction() {
-		class JavaFunction extends EJIFunction {
-			public JavaFunction() {
-				setParameters("fqcn");
-			}
-			
-			private EveObject resolveJavaPackageContainer(String fqcn) {
-				String[] split = fqcn.split("\\.");
-				String className = split[split.length - 1];
-				
-				EveObject pkgContainer = ScopeManager.getVariable(split[0]);
-				if (pkgContainer == null) {
-					pkgContainer = EveObject.prototypeType(split[0]);
-					EveGlobal.addType(split[0], pkgContainer);
-				}
-				
-				EveObject prevContainer = pkgContainer;
-				for (int c = 1; c < split.length - 1; c++) {
-					pkgContainer = pkgContainer.getField(split[c]);
-					if (pkgContainer == null) {
-						pkgContainer = EveObject.prototypeType(split[c]);
-						prevContainer.putField(split[c], pkgContainer);
-					}
-					prevContainer = pkgContainer;
-				}
-				
-				return pkgContainer;
-			}
-
-			@Override
-			public EveObject execute(Map<String, EveObject> parameters) {
-				String className = parameters.get("fqcn").getStringValue();
-				try {
-					Class<?> cl = Class.forName(className);
-					EveObject ctorFunc = EJIHelper.createEJIConstructor(cl);
-					EveObject pkgContainer = resolveJavaPackageContainer(className);
-					
-					if (cl.isAnonymousClass()) {
-						throw new EveError("EJI currently does not support anonymous classes");
-					}
-					
-					if (cl.isArray()) {
-						String simpleName = cl.getSimpleName();
-						simpleName = simpleName.substring(0, simpleName.indexOf("["));
-						simpleName += "_Array";
-						pkgContainer.putField(simpleName, ctorFunc);
-					}
-					else {
-						pkgContainer.putField(cl.getSimpleName(), ctorFunc);
-					}
-				} catch (ClassNotFoundException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-				
-				return null;
-			}
-		}
-		
-		return new EveObject(new JavaFunction());
 	}
 }
