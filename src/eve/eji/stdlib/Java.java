7bb7d5dbb0bae91d212d42e09ed32f0f0508fe25
projectmoon <noone@nowhere.net>
2011-05-15 12:28:34 -0400

    change java::create to be java::expose

--- src/eve/eji/stdlib/Java.java
+++ src/eve/eji/stdlib/Java.java
@@ -5,6 +5,7 @@ import java.lang.reflect.InvocationTargetException;
 import java.util.Map;
 
 import eve.core.EveObject;
+import eve.core.builtins.EveGlobal;
 import eve.eji.EJIFunction;
 import eve.eji.EJIHelper;
 import eve.scope.ScopeManager;
@@ -13,51 +14,28 @@ public class Java {
 	public static void init() {
 		ScopeManager.setNamespace("java");
 		ScopeManager.createGlobalScope();
-		ScopeManager.putVariable("create", javaFunction());
+		ScopeManager.putVariable("expose", javaFunction());
 		ScopeManager.revertNamespace();
 	}
 	
 	private static EveObject javaFunction() {
 		class JavaFunction extends EJIFunction {
 			public JavaFunction() {
-				setParameters("cname", "ctorArgs");
-				setVarargs(true);
-				setVarargsIndex(1);
+				setParameters("className");
 			}
 
 			@Override
 			public EveObject execute(Map<String, EveObject> parameters) {
-				EveObject cname = parameters.get("cname");
-				String className = cname.getStringValue();
-				EveObject ctorArgs = parameters.get("ctorArgs");
-							
+				String className = parameters.get("className").getStringValue();
 				try {
-					Constructor<?> ctor = EJIHelper.findConstructor(Class.forName(className), ctorArgs.getListValue());
-					Object o = ctor.newInstance(EJIHelper.mapToJava(ctorArgs.getListValue()));
-					EveObject eo = EveObject.javaType(o);
-					EJIHelper.mapJavaMethods(eo);
-					return eo;
-				}
-				catch (ClassNotFoundException e) {
+					Class<?> cl = Class.forName(className);
+					EveObject ctorFunc = EJIHelper.createEJIConstructor(cl);
+					EveGlobal.addType(cl.getSimpleName(), ctorFunc);
+				} catch (ClassNotFoundException e) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
-				catch (InstantiationException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-				catch (IllegalAccessException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-				catch (InvocationTargetException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-				catch (SecurityException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
+				
 				return null;
 			}
 		}
