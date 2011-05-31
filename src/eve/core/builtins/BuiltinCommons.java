cde74ae5bfd287a96bbd830eaf42a5aaaf1ff356
projectmoon <noone@nowhere.net>
2011-05-31 14:03:08 -0400

    added BuiltinCommons

---
+++ src/eve/core/builtins/BuiltinCommons.java
@@ -0,0 +1,64 @@
+package eve.core.builtins;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import eve.core.EveError;
+import eve.core.EveObject;
+import eve.core.EveObject.EveType;
+import eve.eji.DynamicField;
+import eve.eji.EJIHelper;
+
+/**
+ * The object prototype, from which everything is cloned.
+ * @author jeff
+ *
+ */
+public class BuiltinCommons {
+	private static final Map<String, EveObject> typePool = new HashMap<String, EveObject>();
+	
+	static {
+		//initialize the type pool with the default (built-in) types.
+		//each type is initialized with common properties by the initialize() method.
+		typePool.put(EveInteger.getPrototype().getTypeName(), initialize(EveInteger.getPrototype()));
+		typePool.put(EveString.getPrototype().getTypeName(), initialize(EveString.getPrototype()));
+		typePool.put(EveDouble.getPrototype().getTypeName(), initialize(EveDouble.getPrototype()));
+		typePool.put(EveBoolean.getPrototype().getTypeName(), initialize(EveBoolean.getPrototype()));
+		typePool.put(EveFunction.getPrototype().getTypeName(), initialize(EveFunction.getPrototype()));
+		typePool.put(EveList.getPrototype().getTypeName(), initialize(EveList.getPrototype()));
+		typePool.put(EveJava.getPrototype().getTypeName(), initialize(EveJava.getPrototype()));
+		typePool.put(EveDictionary.getPrototype().getTypeName(), initialize(EveDictionary.getPrototype()));
+	}
+	
+	public static void addType(String name, EveObject type) {
+		if (typePool.get(name) != null) {
+			throw new EveError("type " + name + " already exists in the global type pool.");
+		}
+		
+		typePool.put(name, type);
+	}
+	
+	public static EveObject getType(String name) {
+		return typePool.get(name);
+	}
+
+	public static EveObject initialize(EveObject prototype) {
+		prototype.putField("type", typeProperty());
+		return prototype;
+	}
+	
+	private static DynamicField typeProperty() {
+		return new DynamicField() {
+			@Override
+			public EveObject get() {
+				EveObject self = EJIHelper.self();
+				return new EveObject(self.getTypeName());
+			}
+
+			@Override
+			public void set(EveObject value) {
+				throw new EveError("type is a read-only property");
+			}
+		};
+	}
+}
