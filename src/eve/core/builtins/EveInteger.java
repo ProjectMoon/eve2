c21311539d837297a631399e0cf8830221857682
projectmoon <noone@nowhere.net>
2011-05-31 13:39:34 -0400

    int.create

--- src/eve/core/builtins/EveInteger.java
+++ src/eve/core/builtins/EveInteger.java
@@ -1,6 +1,10 @@
 package eve.core.builtins;
 
+import java.util.Map;
+
+import eve.core.EveError;
 import eve.core.EveObject;
+import eve.eji.EJIFunction;
 
 /**
  * The int prototype.
@@ -15,8 +19,27 @@ public class EveInteger extends EveObject {
 	}
 	
 	private EveInteger() {
-		//super(EveObjectPrototype.getPrototype());
 		this.setType(EveType.PROTOTYPE);
 		this.setTypeName("int");
+		this.putField("create", new EveObject(new EveIntCreateFunction()));
+	}
+	
+	private class EveIntCreateFunction extends EJIFunction {
+		public EveIntCreateFunction() {
+			this.addParameter("value");
+		}
+		
+		@Override
+		public EveObject execute(Map<String, EveObject> parameters) {
+			EveObject value = parameters.get("value");
+			
+			if (value.getType() != EveType.INTEGER) {
+				throw new EveError("int.create requires an int parameter");
+			}
+			
+			return new EveObject(value.getIntValue());
+		}
+		
+		
 	}
 }
