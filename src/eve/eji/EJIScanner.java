bbdfc5c039c4bd44a45e8aa9b85c5ef1e608ed7a
ProjectMoon <knifed@gmail.com>
2011-09-04 21:08:59 -0400

    EJI scanner changes

--- src/eve/eji/EJIScanner.java
+++ src/eve/eji/EJIScanner.java
@@ -19,6 +19,14 @@ import eve.core.EveError;
 import eve.core.EveObject;
 import eve.core.builtins.BuiltinCommons;
 
+/**
+ * The EJIScanner scans Java packages for types and namespaces. Its main purpose
+ * is to load the standard library and built-in types. It also loads EJI code
+ * created by users. Finally, it provides two static methods for finding namespace
+ * classes.
+ * @author jeff
+ *
+ */
 public class EJIScanner {
 	private static final Map<String, Class<?>> memoizedNamespaces = new HashMap<String, Class<?>>();
 	
@@ -28,43 +36,14 @@ public class EJIScanner {
 		
 	}
 	
-	public void addPackage(String packageName) {
-		packages.add(packageName);
-	}
-	
-	public void scan() {
-		FilterBuilder fb = new FilterBuilder();
-		Set<URL> pkgUrls = new HashSet<URL>();
-		
-		for (String pkg : packages) {
-			fb.include(FilterBuilder.prefix(pkg));
-			pkgUrls.addAll(ClasspathHelper.getUrlsForPackagePrefix(pkg));
-		}
-		
-		ConfigurationBuilder cb =
-			new ConfigurationBuilder()
-			.filterInputsBy(fb)
-			.setUrls(pkgUrls)
-			.setScanners(new TypeAnnotationsScanner().filterResultsBy(fb));
-		
-		Reflections reflections = new Reflections(cb);
-		
-		Set<Class<?>> annotated = reflections.getTypesAnnotatedWith(EJIType.class);
-		try {
-			createEJITypes(annotated);
-		} catch (InstantiationException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IllegalAccessException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IntrospectionException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
-	
-	public Class<?> findNamespace(String pkg, String namespace) {
+	/**
+	 * Find a namespace in a package. Does not load the namespace; only returns
+	 * the class. Use {@link EJIHelper#createEJINamespace(Class)} for that.
+	 * @param pkg
+	 * @param namespace
+	 * @return The class, if found. Null otherwise.
+	 */
+	public static Class<?> findNamespace(String pkg, String namespace) {
 		//don't need to scan all the time...
 		if (memoizedNamespaces.containsKey(pkg + ":" + namespace)) {
 			return memoizedNamespaces.get(pkg + ":" + namespace);
@@ -102,10 +81,76 @@ public class EJIScanner {
 		}		
 	}
 	
-	public Class<?> findStandardNamespace(String namespace) {
+	/**
+	 * Find a standard namespace (from eve.eji.stdlib).Does not load the namespace; only returns
+	 * the class. Use {@link EJIHelper#createEJINamespace(Class)} for that.
+	 * @param namespace
+	 * @return The class, if found. Null otherwise.
+	 */
+	public static Class<?> findStandardNamespace(String namespace) {
 		return findNamespace("eve.eji.stdlib", namespace);
 	}
 	
+	/**
+	 * Add a package to scan to this scanner.
+	 * @param packageName
+	 */
+	public void addPackage(String packageName) {
+		packages.add(packageName);
+	}
+	
+	/**
+	 * Creates a Reflections scanner based on the configured packages.
+	 * @return the scanner.
+	 */
+	private Reflections createScanner() {
+		FilterBuilder fb = new FilterBuilder();
+		Set<URL> pkgUrls = new HashSet<URL>();
+		
+		for (String pkg : packages) {
+			fb.include(FilterBuilder.prefix(pkg));
+			pkgUrls.addAll(ClasspathHelper.getUrlsForPackagePrefix(pkg));
+		}
+		
+		ConfigurationBuilder cb =
+			new ConfigurationBuilder()
+			.filterInputsBy(fb)
+			.setUrls(pkgUrls)
+			.setScanners(new TypeAnnotationsScanner().filterResultsBy(fb));
+		
+		return new Reflections(cb);
+	}
+	
+	/**
+	 * Scan the configured packages for EJI Types, and either add them to
+	 * the type pool (if they are marked with EJIBuiltin), or to the external
+	 * types map (if they are not marked).
+	 */
+	public void scanForTypes() {
+		Reflections reflections = createScanner();
+		Set<Class<?>> annotated = reflections.getTypesAnnotatedWith(EJIType.class);
+		
+		try {
+			createEJITypes(annotated);
+		} catch (InstantiationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IntrospectionException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	/**
+	 * Process all found EJITypes from a scanForTypes().
+	 * @param types
+	 * @throws InstantiationException
+	 * @throws IllegalAccessException
+	 * @throws IntrospectionException
+	 */
 	private void createEJITypes(Set<Class<?>> types) throws InstantiationException, IllegalAccessException, IntrospectionException {
 		for (Class<?> type : types) {
 			EJIType typeInfo = type.getAnnotation(EJIType.class);
@@ -123,6 +168,24 @@ public class EJIScanner {
 			else {
 				ExternalTypes.addType(typeInfo.value(), eo);
 			}
+		}
+	}
+	
+	/**
+	 * Scan the configured packages for EJI Namespaces, and add them into
+	 * the interpreter environment.
+	 */
+	public void loadNamespaces() {
+		//Due to a bug with the library, we must search *above*
+		//eve.eji.stdlib. All standard namespaces are in eve.eji.stdlib
+		//however. If we were to search eve.eji.stdlib, we would get no
+		//classes.
+		Reflections r = createScanner();
+		
+		Set<Class<?>> annotated = r.getTypesAnnotatedWith(EJINamespace.class);
+		
+		for (Class<?> cl : annotated) {
+			EJIHelper.createEJINamespace(cl);
 		}
 	}
 }
