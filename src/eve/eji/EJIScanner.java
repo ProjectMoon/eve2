092498aa995580dd92c0712c517bad30a3fa0819
ProjectMoon <knifed@gmail.com>
2011-09-21 18:53:57 -0400

    process builtins before non builtins

--- src/eve/eji/EJIScanner.java
+++ src/eve/eji/EJIScanner.java
@@ -1,19 +1,8 @@
 package eve.eji;
 
 import java.beans.IntrospectionException;
-import java.net.URL;
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.HashSet;
-import java.util.List;
-import java.util.Map;
 import java.util.Set;
-
-import org.reflections.Reflections;
-import org.reflections.scanners.TypeAnnotationsScanner;
-import org.reflections.util.ClasspathHelper;
-import org.reflections.util.ConfigurationBuilder;
-import org.reflections.util.FilterBuilder;
 
 import com.impetus.annovention.ClasspathDiscoverer;
 import com.impetus.annovention.Discoverer;
@@ -39,7 +28,8 @@ public class EJIScanner {
 	/**
 	 * Scan the classpath for EJI types, and either add them to the type pool
 	 * (if they are marked with EJIBuiltin), or to the external types map
-	 * (if they are not marked).
+	 * (if they are not marked). Builtins are always created before external
+	 * types.
 	 */	
 	public void scanForTypes() {
 		final Set<String> foundTypes = new HashSet<String>();
@@ -57,19 +47,30 @@ public class EJIScanner {
 		
 		d.discover(true, false, false, true, false);
 		
-		Set<Class<?>> types = new HashSet<Class<?>>();
+		
+		Set<Class<?>> builtins = new HashSet<Class<?>>();
+		Set<Class<?>> externals = new HashSet<Class<?>>();
 		
 		for (String className : foundTypes) {
 			try {
-				types.add(Class.forName(className));
+				Class<?> type = Class.forName(className);
+				
+				if (type.isAnnotationPresent(EJIBuiltinType.class)) {
+					builtins.add(type);
+				}
+				else {
+					externals.add(type);
+				}
+				
 			}
 			catch (ClassNotFoundException e) {
 				throw new EveError("EJI error: EJI class not found: " + e.getMessage());
 			}
 		}
-		
+				
 		try {
-			createEJITypes(types);
+			createEJITypes(builtins);
+			createEJITypes(externals);
 		}
 		catch (InstantiationException e) {
 			// TODO Auto-generated catch block
@@ -93,21 +94,18 @@ public class EJIScanner {
 	 * @throws IntrospectionException
 	 */
 	private void createEJITypes(Set<Class<?>> types) throws InstantiationException, IllegalAccessException, IntrospectionException {
+		//need to process these in order... builtins before anything else.
+		
 		for (Class<?> type : types) {
 			EJIType typeInfo = type.getAnnotation(EJIType.class);
-			EveObject ctor = null;
 			
 			//bypass type coercion if the type specifies it.
 			boolean bypassTypeCoercion = false;
+			
 			if (type.isAnnotationPresent(EJINoCoerce.class)) {
-				//ctor = EJIHelper.createEJIConstructor(type, true);
 				bypassTypeCoercion = true;
 			}
-			else {
-				//ctor = EJIHelper.createEJIConstructor(type, false);
-			}
 			
-			//EveObject eveType = EJIHelper.createEJIType(typeInfo.value(), ctor);
 			EveObject eveType = EJIHelper.createEJIType(type, bypassTypeCoercion);
 			
 			//whether or not we should consider this a built-in type.
